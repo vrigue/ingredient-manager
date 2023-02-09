dde067c2f32ad585e393e47edd53cb61366d9bc3
vrigue <vrielleguevarra@gmail.com>
2023-02-09 00:41:28 -0500

    completed up to 5.3, fixed favicon

--- app.js
+++ app.js
@@ -1,31 +1,118 @@
-//set up the server
+/* set up the server */
 const express = require( "express" );
 const logger = require("morgan");
 const app = express();
 const port = 8080;
+const db = require("./db/db_connection");
+app.set( "views",  __dirname + "/views");
+app.set( "view engine", "ejs" );
 
-// define middleware that logs all incoming requests
+/* define middleware that logs all incoming requests */
 app.use(logger("dev"));
 
-// define middleware that serves static resources in the public directory
+/* define middleware that serves static resources in the public directory */
 app.use(express.static(__dirname + '/public'));
 
-// define a route for the default home page
+
+/* define a route for the default home page */
 app.get( "/", ( req, res ) => {
-    res.sendFile(__dirname + "/views/index.html");
+    res.render("index");
 } );
 
-// define a route for the inventory page
+
+const read_ingredient_all_sql = `
+SELECT
+    id, item, quantity, purchaseQuantity, description
+FROM
+    ingredient;
+`
+
+/* define a route for the inventory page */
 app.get( "/inventory", ( req, res ) => {
-    res.sendFile( __dirname + "/views/inventory.html" );
+    db.execute(read_ingredient_all_sql, (error, results) => {
+        if (error) {
+            res.status(500).send(error); /* sends an internal server error if something goes wrong */
+        } else {
+            res.render("inventory", {ingredient : results});
+        }
+    });
 } );
 
-// define a route for the ingredient detail page
-app.get( "/inventory/ingredient", ( req, res ) => {
-    res.sendFile( __dirname + "/views/ingredient.html" );
-} );
 
-// start the server
+const read_ingredient_sql = `
+    SELECT
+        id, item, quantity, purchaseQuantity, description
+    FROM
+        ingredient
+    WHERE 
+        id = ?;
+`
+
+const read_ingredient_stock_sql = `
+    SELECT
+        ingredient_id, expiration_date, brand_name, price
+    FROM
+        stock
+    WHERE 
+        ingredient_id = ?;
+`
+
+/* define a route for the ingredient detail page */
+app.get( "/inventory/ingredient/:id", ( req, res ) => {
+    db.execute(read_ingredient_sql, [req.params.id], (error, results1) => {
+        if (error) {
+            res.status(500).send(error); /* sends an internal server error if something goes wrong */
+        } else if (results1.length === 0) {
+            res.status(404).send(`No ingredient found with id = '${req.params.id}`)
+        }  else {
+            db.execute(read_ingredient_stock_sql, [req.params.id], (error, results2) => {
+                if (error) {
+                    res.status(500).send(error); /* sends an internal server error if something goes wrong */
+                } else if (results2.length === 0) {
+                    res.status(404).send(`No stock found with ingredient_id = '${req.params.id}`)
+                }  else {
+                    res.render("ingredient", {data : {ingredient : results1[0], stock : results2}});
+                }
+            });
+        }
+    });
+});
+
+
+const delete_ingredient_sql = `
+    DELETE 
+        FROM
+            ingredient
+        WHERE
+            id = ?
+`
+
+const delete_ingredient_stock_sql = `
+    DELETE 
+        FROM
+            stock
+        WHERE
+            ingredient_id = ?
+`
+
+/* define a route for item DELETE */
+app.get("/inventory/ingredient/:id/delete", ( req, res ) => {
+    db.execute(delete_ingredient_stock_sql, [req.params.id], (error, results1) => {
+        if (error)
+            res.status(500).send(error); /* sends an internal server error if something goes wrong */
+        else {
+            db.execute(delete_ingredient_sql, [req.params.id], (error, results2) => {
+                if (error)
+                    res.status(500).send(error); /* sends an internal server error if something goes wrong */
+                else {
+                    res.redirect("/inventory");
+                }
+            });
+        }
+    });
+})
+
+/* start the server */
 app.listen( port, () => {
     console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
 } );
--- db/db_init.js
+++ db/db_init.js
@@ -1,40 +1,77 @@
 // (Re)Sets up the database, including a little bit of sample data
 const db = require("./db_connection");
 
-/**** Delete existing table, if any ****/
+/**** Delete existing tables, if any ****/
 const drop_ingredient_table_sql = "DROP TABLE IF EXISTS `ingredient`;"
+const drop_stock_table_sql = "DROP TABLE IF EXISTS `stock`;"
+
+db.execute(drop_stock_table_sql);
 db.execute(drop_ingredient_table_sql);
 
-/**** Create "ingredient" table (again)  ****/
+/**** Create "inventory" and "stock" tables (again)  ****/
 const create_ingredient_table_sql = `
     CREATE TABLE ingredient (
         id INT NOT NULL AUTO_INCREMENT,
         item VARCHAR(45) NOT NULL,
         quantity INT NOT NULL,
         purchaseQuantity INT NOT NULL,
+        description VARCHAR(1000) NOT NULL,
         PRIMARY KEY (id)
     );
 `
+
+const create_stock_table_sql = `
+    CREATE TABLE stock (
+        id INT NOT NULL AUTO_INCREMENT,
+        expiration_date VARCHAR(10) NOT NULL,
+        brand_name VARCHAR(45) NOT NULL,
+        price VARCHAR(45) NOT NULL,
+        ingredient_id INT NOT NULL,
+        PRIMARY KEY (id),
+        INDEX ingredient_id_idx (ingredient_id ASC),
+        CONSTRAINT ingredient_id
+          FOREIGN KEY (ingredient_id)
+          REFERENCES ingredient (id)
+          ON DELETE RESTRICT
+          ON UPDATE CASCADE
+    );   
+`
 db.execute(create_ingredient_table_sql);
+db.execute(create_stock_table_sql);
 
-/**** Create some sample items ****/
 
+/**** Create some sample ingredients ****/
 const insert_ingredient_table_sql = `
     INSERT INTO ingredient 
-        (item, quantity, purchaseQuantity) 
+        (item, quantity, purchaseQuantity, description) 
     VALUES 
-        (?, ?, ?);
+        (?, ?, ?, ?);
 `
-db.execute(insert_ingredient_table_sql, ['Dark Brown Sugar', '2', '4']);
+db.execute(insert_ingredient_table_sql, ['Dark Brown Sugar', '2', '4', 'Dark brown sugar has more molasses than light brown sugar, so your chocolate chip cookies and other baked goods will come out much darker in color.']);
 
-db.execute(insert_ingredient_table_sql, ['Bread Flour', '0', '6']);
+db.execute(insert_ingredient_table_sql, ['Bread Flour', '0', '6', 'Bread flour has more gluten or protein than regular all-purpose flour.']);
 
-db.execute(insert_ingredient_table_sql, ['White Sugar', '12', '0']);
+db.execute(insert_ingredient_table_sql, ['White Sugar', '12', '0', 'White sugar is arguably the most used ingredient in baking.']);
 
-db.execute(insert_ingredient_table_sql, ['Ube Powder', '4', '2']);
+db.execute(insert_ingredient_table_sql, ['Ube Powder', '4', '2', 'Great for making any ube dessert, especially ube cheese pandesal!']);
 
-/**** Read the sample items inserted ****/
 
+/**** Create some sample items ****/
+const insert_stock_table_sql = `
+    INSERT INTO stock 
+        (expiration_date, brand_name, price, ingredient_id) 
+    VALUES 
+        (?, ?, ?, ?);
+`
+
+db.execute(insert_stock_table_sql, ['2023-02-28', 'Lidl', '1.75', '1']);
+
+db.execute(insert_stock_table_sql, ['2023-04-15', 'Aldi', '1.25', '1']);
+
+db.execute(insert_stock_table_sql, ['2023-07-26', 'ShopRite', '2.10', '1']);
+
+
+/**** Read the sample ingredients inserted ****/
 const read_ingredient_table_sql = "SELECT * FROM ingredient";
 
 db.execute(read_ingredient_table_sql, 
@@ -43,6 +80,19 @@ db.execute(read_ingredient_table_sql,
             throw error;
 
         console.log("Table 'ingredient' initialized with:")
+        console.log(results);
+    }
+);
+
+/**** Read the sample items inserted ****/
+const read_stock_table_sql = "SELECT * FROM stock";
+
+db.execute(read_stock_table_sql, 
+    (error, results) => {
+        if (error) 
+            throw error;
+
+        console.log("Table 'stock' initialized with:")
         console.log(results);
     }
 );
